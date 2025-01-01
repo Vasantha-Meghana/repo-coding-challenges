5c932d5c5acf19c5cb046a62cbedb8f5ced62a0d
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-01-01 08:05:33 +0530

    Created Maximum Average Subarray 1.java

---
+++ coding/1st January 2025/Maximum Average Subarray 1.java
@@ -0,0 +1,19 @@
+//Maximum Average Subarray 1
+class Solution 
+{
+    public double findMaxAverage(int[] nums, int k) 
+    {
+        int n = nums.length;
+        int sum = 0;
+        for(int i = 0; i < k; i++)
+            sum = sum + nums[i];
+        int maxsum = sum;
+        for(int i = k; i < n; i++)
+        {
+            sum = sum + nums[i] - nums[i - k];
+            if(sum > maxsum)
+                maxsum = sum;
+        }
+        return (double)maxsum / k;
+    }
+}
