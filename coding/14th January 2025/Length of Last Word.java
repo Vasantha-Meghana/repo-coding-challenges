fa59cc2748c30292d232a39d168cfa17eab8d515
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-01-14 09:26:54 +0530

    Create Length of Last Word.java

---
+++ coding/14th January 2025/Length of Last Word.java
@@ -0,0 +1,9 @@
+//Length of Last Word
+class Solution 
+{
+    public int lengthOfLastWord(String s) 
+    {
+        String[] arr = s.split(" ");
+        return arr[arr.length - 1].length();
+    }
+}
