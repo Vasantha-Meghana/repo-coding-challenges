cc763370bb9da932a4b5b956844a61d4dc6850f2
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-02-17 20:54:51 +0530

    Create Implement Queue using Stacks.java

---
+++ coding/17th February 2025/Implement Queue using Stacks.java
@@ -0,0 +1,37 @@
+//Implement Queue using Stacks
+class MyQueue 
+{
+    Queue<Integer> que;
+    public MyQueue() 
+    {
+        que = new LinkedList<>();
+    }
+    
+    public void push(int x) 
+    {
+        que.add(x);
+    }
+    
+    public int pop() 
+    {
+        return que.poll();
+    }
+    
+    public int peek() 
+    {
+        return que.peek();
+    }
+    
+    public boolean empty() {
+        return que.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
