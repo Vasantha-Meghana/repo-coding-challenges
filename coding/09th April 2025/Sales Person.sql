8ad1088f5bc46baf4dd6d8bb54c0a6dcc5678f0a
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-04-09 21:08:27 +0530

    Create Sales Person.sql

---
+++ coding/09th April 2025/Sales Person.sql
@@ -0,0 +1,8 @@
+# Sales Person
+SELECT s.name 
+FROM SaleSperson s
+WHERE s.name NOT IN (SELECT s.name 
+FROM SalesPerson s
+LEFT JOIN Orders o ON s.sales_id = o.sales_id
+LEFT JOIN Company c ON o.com_id = c.com_id
+WHERE c.name = 'Red');
