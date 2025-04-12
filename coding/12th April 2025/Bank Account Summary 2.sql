bdcc53bc15dc795249f3394fdc501c2d28a72780
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-04-12 15:08:02 +0530

    Create Bank Account Summary 2.sql

---
+++ coding/12th April 2025/Bank Account Summary 2.sql
@@ -0,0 +1,6 @@
+# Bank Account Summary 2
+SELECT u.name AS name, SUM(amount) AS balance
+FROM Users AS u JOIN Transactions AS t
+ON u.account = t.account
+GROUP BY u.account
+HAVING balance > 10000;
