67d28929a72a338437e4d8d0d2190d882e7fa830
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-05-06 08:41:46 +0530

    Create Monthly Transactions 1.sql

---
+++ coding/06th May 2025/Monthly Transactions 1.sql
@@ -0,0 +1,8 @@
+# Monthly Transactions 1
+SELECT LEFT(trans_date, 7) AS month, country, 
+        COUNT(id) AS trans_count,
+        SUM(state a= 'approved') AS approved_count,
+        SUM(amount) AS trans_total_amount,
+        SUM((state = 'approved') * amount) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country
