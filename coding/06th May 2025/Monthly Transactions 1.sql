a9c73d75dda44ae65314fdc8fbc798d3aac7e25c
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-05-06 08:42:44 +0530

    Update Monthly Transactions 1.sql

--- coding/06th May 2025/Monthly Transactions 1.sql
+++ coding/06th May 2025/Monthly Transactions 1.sql
@@ -1,7 +1,7 @@
 # Monthly Transactions 1
 SELECT LEFT(trans_date, 7) AS month, country, 
         COUNT(id) AS trans_count,
-        SUM(state a= 'approved') AS approved_count,
+        SUM(state = 'approved') AS approved_count,
         SUM(amount) AS trans_total_amount,
         SUM((state = 'approved') * amount) AS approved_total_amount
 FROM Transactions
