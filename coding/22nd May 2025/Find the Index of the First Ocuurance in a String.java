9599b6633cc3f19333a107bd67c6853b56d8a9b6
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-05-22 17:05:06 +0530

    Create Find the Index of the First Ocuurance in a String.java

---
+++ coding/22nd May 2025/Find the Index of the First Ocuurance in a String.java
@@ -0,0 +1,13 @@
+//Find the Index of the First Ocuurance in a String
+class Solution 
+{
+    public int strStr(String haystack, String needle) 
+    {
+        for(int i = 0, j = needle.length(); j <= haystack.length(); i++, j++)
+        {
+            if(haystack.substring(i, j).equals(needle))
+                return i;
+        } 
+        return -1;
+    }
+}
