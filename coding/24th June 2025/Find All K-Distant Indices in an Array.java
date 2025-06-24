74918f93796ad1fc8ab73a25142382444dcb8011
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-06-24 13:33:40 +0530

    Create Find All K-Distant Indices in an Array.java

---
+++ coding/24th June 2025/Find All K-Distant Indices in an Array.java
@@ -0,0 +1,20 @@
+//Find All K-Distant Indices in an Array
+class Solution 
+{
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) 
+    {
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++)
+        {
+            for(int j = 0; j < nums.length; j++)
+            {
+                if(nums[j] == key && Math.abs(i - j) <= k)
+                {
+                    res.add(i);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
