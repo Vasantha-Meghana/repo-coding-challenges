c35a671cb1fe433fbbdcf1183d00a59c413c9097
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-07-11 17:51:46 +0530

    Create Make Sum Divisible by P.java

---
+++ coding/11th July 2025/Make Sum Divisible by P.java
@@ -0,0 +1,29 @@
+//Make Sum Divisible by P
+class Solution 
+{
+    public int minSubarray(int[] nums, int p) 
+    {
+        long total = 0;
+        for(int ele : nums)
+            total += ele;
+        int rem = (int) (total % p);
+        if(rem == 0)   
+            return 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        long prefixsum = 0;
+        int minLength = nums.length;
+        for(int i = 0; i < nums.length; i++)
+        {
+            prefixsum += nums[i];
+            int curr = (int) (prefixsum % p);
+            int target = (curr - rem + p) % p;
+            if(map.containsKey(target))
+                minLength = Math.min(minLength, i - map.get(target));
+            map.put(curr, i);
+        }
+        if(minLength == nums.length)
+            return -1;
+        return minLength;
+    }
+}
