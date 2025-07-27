5061e48d6e89429659677af1482e1234e317f238
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-07-27 16:00:27 +0530

    Create Number of Laser Beams in a Bank.java

---
+++ coding/27th July 2025/Number of Laser Beams in a Bank.java
@@ -0,0 +1,21 @@
+// Number of Laser Beams in a Bank
+class Solution 
+{
+    public int numberOfBeams(String[] bank) 
+    {
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(String str : bank)
+        {
+            int count = 0;
+            for(char ch : str.toCharArray())
+                if(ch == '1')
+                    count++;
+            if(count != 0)
+                arr.add(count);
+        }
+        int result = 0;
+        for(int i = 1; i < arr.size(); i++)
+            result += (arr.get(i) * arr.get(i - 1));
+        return result;
+    }
+}
