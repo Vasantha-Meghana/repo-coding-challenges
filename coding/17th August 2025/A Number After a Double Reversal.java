7a1607f5db984336706d245806d5e067abdd5930
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-08-17 08:47:56 +0530

    Create A Number After a Double Reversal.java

---
+++ coding/17th August 2025/A Number After a Double Reversal.java
@@ -0,0 +1,10 @@
+// A Number After a Double Reversal
+class Solution 
+{
+    public boolean isSameAfterReversals(int num) 
+    {
+        if(num == 0)   return true;
+        if(num % 10 == 0)   return false;
+        return true;
+    }
+}
