9543a1746a99c545d4f6e90c14c232421d0e950e
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-08-22 13:39:43 +0530

    Create Minimum Operations to Exceed Threshold Value 1.java

---
+++ coding/22nd August 2025/Minimum Operations to Exceed Threshold Value 1.java
@@ -0,0 +1,14 @@
+//Minimum Operations to Exceed Threshold Value 1
+class Solution 
+{
+    public int minOperations(int[] nums, int k) 
+    {
+        int result = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] < k)
+                result++;
+        }
+        return result;
+    }
+}
