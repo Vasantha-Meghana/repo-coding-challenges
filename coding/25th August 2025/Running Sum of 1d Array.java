d298e658dfb072acc31e40d8d4ebfa10a63dfa13
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-08-25 10:35:23 +0530

    Create Running Sum of 1d Array.java

---
+++ coding/25th August 2025/Running Sum of 1d Array.java
@@ -0,0 +1,15 @@
+//Running Sum of 1d Array
+class Solution 
+{
+    public int[] runningSum(int[] nums) 
+    {
+        int sum = 0;
+        int[] result = new int[nums.length];
+        for(int i = 0; i < nums.length; i++)
+        {
+            sum += nums[i];
+            result[i] = sum;
+        }
+        return result;
+    }
+}
