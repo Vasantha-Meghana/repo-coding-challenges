2dbcb7f6aff425875ce512f34828ba3b84414fea
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-09-02 18:45:34 +0530

    Create Defanging an IP Address.java

---
+++ coding/02nd September 2025/Defanging an IP Address.java
@@ -0,0 +1,16 @@
+//Defanging an IP Address
+class Solution 
+{
+    public String defangIPaddr(String address) 
+    {
+        StringBuilder result = new StringBuilder();
+        for(char ch : address.toCharArray())
+        {
+            if(ch == '.')
+                result.append("[.]");
+            else
+                result.append(ch);
+        }
+        return result.toString();
+    }
+}
