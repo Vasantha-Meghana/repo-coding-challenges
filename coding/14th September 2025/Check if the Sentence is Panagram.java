3358ae3e38a656764d82b1cb3dc19973b4c36ed2
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-09-14 12:14:55 +0530

    Create Check if the Sentence is Panagram.java

---
+++ coding/14th September 2025/Check if the Sentence is Panagram.java
@@ -0,0 +1,16 @@
+//Check if the Sentence is Panagram
+class Solution 
+{
+    public boolean checkIfPangram(String sentence) 
+    {
+        int[] arr = new int[26];
+        for(char ch : sentence.toCharArray())
+            arr[ch - 'a']++;
+        for(int i = 0; i < 26; i++)
+        {
+            if(arr[i] == 0)
+                return false;
+        }
+        return true;
+    }
+}
