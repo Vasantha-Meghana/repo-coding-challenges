b980c31c5a1ed14e5ebee5b8d751d48e30d93bc2
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-09-16 18:21:42 +0530

    Create Number of Arithmetic Triplets.java

---
+++ coding/16th September 2025/Number of Arithmetic Triplets.java
@@ -0,0 +1,23 @@
+//Number of Arithmetic Triplets
+class Solution 
+{
+    public int arithmeticTriplets(int[] nums, int diff) 
+    {
+        int result = 0;
+        for(int i = 0; i < nums.length - 2; i++)
+        {
+            for(int j = i + 1; j < nums.length - 1; j++)
+            {
+                if(nums[j] - nums[i] == diff)
+                {
+                    for(int k = j + 1; k < nums.length; k++)
+                    {
+                        if(nums[k] - nums[j] == diff)
+                            result++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
