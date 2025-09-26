8edfb31a3880bec92e34b75afb3bc2bba9e84115
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-09-26 17:34:10 +0530

    Create Merge Two Sorted Lists.java

---
+++ coding/26th September 2025/Merge Two Sorted Lists.java
@@ -0,0 +1,50 @@
+//Merge Two Sorted Lists
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution 
+{
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) 
+    {
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        while(list1 != null && list2 != null)
+        {
+            ListNode newNode;
+            if(list1.val <= list2.val)
+            {
+                newNode = new ListNode(list1.val);
+                list1 = list1.next;
+            }
+            else
+            {
+                newNode = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            temp.next = newNode;
+            temp = temp.next;
+        }
+        while(list1 != null)
+        {
+            ListNode newNode = new ListNode(list1.val);
+            temp.next = newNode;
+            temp = temp.next;
+            list1 = list1.next;
+        }
+        while(list2 != null)
+        {
+            ListNode newNode = new ListNode(list2.val);
+            temp.next = newNode;
+            temp = temp.next;
+            list2 = list2.next;
+        }
+        return head.next;
+    }
+}
