e7941e445c565a63638d1987277eb7edcdf586dd
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-10-07 19:19:40 +0530

    Create Concatinantion of Array.java

---
+++ coding/07th October 2025/Concatinantion of Array.java
@@ -0,0 +1,31 @@
+//Concatinantion of Array
+class Solution 
+{
+    public int[] getConcatenation(int[] nums) 
+    {
+        int n = nums.length;
+        int[] result = new int[2 * n];
+        for(int i = 0; i < n; i++)
+        {
+            result[i] = nums[i];
+            result[i + n] = nums[i];
+        }
+        return result;
+    }
+}
+/*
+class Solution 
+{
+    public int[] getConcatenation(int[] nums) 
+    {
+        int n = nums.length;
+        int ans[] = new int[2 * n];
+        for(int i = 0; i < n; i++)
+        {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+}
+*/
