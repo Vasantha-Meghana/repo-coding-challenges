bd598f02d920f528ccda1384329ddd56449e7bc6
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-10-13 17:34:08 +0530

    Create Longest Common Prefix.java

---
+++ coding/13th October 2025/Longest Common Prefix.java
@@ -0,0 +1,68 @@
+//Longest Common Prefix
+class Solution 
+{
+    public String longestCommonPrefix(String[] strs) 
+    {
+        Arrays.sort(strs);
+        String shortstr = strs[0];
+        String longstr = strs[strs.length - 1];
+        int i = 0;
+        for(i = 0; i < shortstr.length(); i++)
+        {
+            if(shortstr.charAt(i) != longstr.charAt(i))
+                break;
+        }
+        return shortstr.substring(0, i);
+    }
+}
+/*
+class Solution 
+{
+    public String longestCommonPrefix(String[] strs) 
+    {
+        Arrays.sort(strs);
+        StringBuilder res = new StringBuilder();
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        int len = first.length();
+        if(last.length() < len)
+            len = last.length();
+        for(int i = 0; i < len; i++)
+        {
+            if(first.charAt(i) != last.charAt(i))
+                return res.toString();
+            res.append(first.charAt(i));
+        }
+        return res.toString();
+    }
+}
+*/
+
+/*
+class Solution 
+{
+    public String longestCommonPrefix(String[] strs) 
+    {
+        int n = strs.length;
+        String res = "";
+        String shortstr = strs[0];
+        for(int i = 1; i < n; i++)
+        {
+            if(strs[i].length() < shortstr.length())
+            {
+                shortstr = strs[i];
+            }
+        }
+        for(int k = 0; k < shortstr.length(); k++)
+        {
+            for(String s : strs)
+            {
+                if(s.charAt(k) != shortstr.charAt(k))
+                    return res;    
+            }
+            res = res + shortstr.charAt(k);
+        }
+        return res;
+    }
+}
+*/
