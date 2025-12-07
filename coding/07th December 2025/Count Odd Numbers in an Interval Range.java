ffd0817e808397c5dbbbef92ced5b335465f44a4
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-12-07 11:35:05 +0530

    Create Count Odd Numbers in an Interval Range.java

---
+++ coding/07th December 2025/Count Odd Numbers in an Interval Range.java
@@ -0,0 +1,32 @@
+//Count Odd Numbers in an Interval Range
+class Solution 
+{
+    public int countOdds(int low, int high) 
+    {
+        int diff = high - low + 1;
+        if(low % 2 != 0 && high % 2 != 0)
+            return (diff / 2) + 1;
+        return diff / 2;
+    }
+}
+/*
+class Solution 
+{
+    public int countOdds(int low, int high) 
+    {
+        if(low % 2 != 0 && high % 2 == 0)
+                high = high - 1;
+        if(low % 2 == 0)
+        {
+            if(high % 2 != 0)
+                low = low + 1;
+            else
+            {
+                low = low + 1;
+                high = high - 1;
+            }
+        }
+        return ((high - low) / 2) + 1;
+    }
+}
+*/
