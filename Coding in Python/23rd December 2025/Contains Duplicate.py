3a9969a8caf956c8a667e1ef8b9f0e2d8163e46d
Vasantha-Meghana <meghanakotu04@gmail.com>
2025-12-23 20:41:32 +0530

    Create Contains Duplicate.py

---
+++ Coding in Python/23rd December 2025/Contains Duplicate.py
@@ -0,0 +1,9 @@
+#Contains Duplicate
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for ele in nums:
+            if ele in s:
+                return True
+            s.add(ele)
+        return False
