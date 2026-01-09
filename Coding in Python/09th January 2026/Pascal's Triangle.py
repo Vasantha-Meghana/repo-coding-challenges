9b95deec155fb62782a40aba177681ace3995fdb
Vasantha-Meghana <meghanakotu04@gmail.com>
2026-01-09 12:05:10 +0530

    Create Pascal's Triangle.py

---
+++ Coding in Python/09th January 2026/Pascal's Triangle.py
@@ -0,0 +1,11 @@
+#Pascal's Triangle
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+        for i in range(numRows - 1):
+            dummy_row = [0] + res[-1] + [0]
+            new_row = []
+            for j in range(len(dummy_row) - 1):
+                new_row.append(dummy_row[j] + dummy_row[j + 1])
+            res.append(new_row)
+        return res
