d5378449cfda92eef79c77eef33847dc1d2e0eb8
Vasantha-Meghana <meghanakotu04@gmail.com>
2026-02-13 20:17:03 +0530

    Create Monotonic Array.py

---
+++ Coding in Python/13th February 2026/Monotonic Array.py
@@ -0,0 +1,11 @@
+#Monotonic Array
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        inc = True
+        dec = True
+        for i in range(1, len(nums)):
+            if(nums[i - 1] > nums[i]):
+                inc = False
+            if(nums[i - 1] < nums[i]):
+                dec = False
+        return inc or dec
