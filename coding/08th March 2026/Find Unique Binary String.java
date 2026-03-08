0baed9e5cb90d2b7846c35b6bf7bb668c741263b
Vasantha-Meghana <meghanakotu04@gmail.com>
2026-03-08 20:44:56 +0530

    Create Find Unique Binary String.java

---
+++ coding/08th March 2026/Find Unique Binary String.java
@@ -0,0 +1,16 @@
+//Find Unique Binary String
+class Solution 
+{
+    public String findDifferentBinaryString(String[] nums) 
+    {
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i].charAt(i) == '1')
+                result.append('0');
+            else
+                result.append('1');
+        }
+        return result.toString();
+    }
+}
