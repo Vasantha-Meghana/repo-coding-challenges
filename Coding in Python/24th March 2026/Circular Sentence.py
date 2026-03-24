9b43237e2c862caa76317b5c0458e4acab0e85f6
Vasantha-Meghana <meghanakotu04@gmail.com>
2026-03-24 21:17:57 +0530

    Create Circular Sentence.py

---
+++ Coding in Python/24th March 2026/Circular Sentence.py
@@ -0,0 +1,10 @@
+#Circular Sentence
+class Solution:
+    def isCircularSentence(self, sentence: str) -> bool:
+        words = sentence.split()
+        for i in range(1, len(words)):
+            if words[i - 1][-1] != words[i][0]:
+                return False
+        if words[0][0] != words[-1][-1]:
+            return False
+        return True
