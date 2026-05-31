5ccb6c784da104d367c4e04dbcbfe80982a29204
Vasantha-Meghana <meghanakotu04@gmail.com>
2026-05-31 22:39:06 +0530

    Create Destroying Astroids.java

---
+++ coding/31st May 2026/Destroying Astroids.java
@@ -0,0 +1,15 @@
+class Solution 
+{
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) 
+    {
+        Arrays.sort(asteroids);
+        long curMass = mass;
+        for(int val : asteroids)
+        {
+            if(val > curMass)
+                return false;
+            curMass += val;
+        }
+        return true;
+    }
+}
